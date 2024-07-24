11b719cbbcf93ef9d8398ff501f040a7e517173d
MeseryGG <meserygamer@gmail.com>
2024-07-24 12:52:54 +0300

    Add: Template for users endpoints

---
+++ ProjectTirUsersMicroservice.API/Contracts/UsersEndpoints/Response/UserResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectTirUsersMicroservice.API.Contracts.UsersEndpoints.Response
+{
+    [Serializable]
+    public class UserResponse
+    {
+        public string Name { get; set; } = null!;
+
+        public string Surname { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
+        public DateOnly BirthdayDate { get; set; }
+    }
+}
---
+++ ProjectTirUsersMicroservice.API/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using ProjectTirUsersMicroservice.API.Contracts.UsersEndpoints.Response;
+
+namespace ProjectTirUsersMicroservice.API.Endpoints
+{
+    public static class UsersEndpoints
+    {
+        public static IEndpointRouteBuilder AddUsersEndpoints(this IEndpointRouteBuilder builder) 
+        {
+            var group = builder.MapGroup("Users");
+
+            group.MapGet("GetUserById", GetUserById);
+
+            return builder;
+        }
+
+        private static async Task<UserResponse> GetUserById()
+        {
+            return new UserResponse();
+        }
+    }
+}
