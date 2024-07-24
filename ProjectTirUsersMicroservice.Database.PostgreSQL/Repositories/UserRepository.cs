1bca3e1d623e69e982894db6f0e6fbbdbe291540
MeseryGG <meserygamer@gmail.com>
2024-07-25 02:01:24 +0300

    Change: Database project name

---
+++ ProjectTirUsersMicroservice.Database.PostgreSQL/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTirUsersMicroservice.Core.DomainEntities;
+using ProjectTirUsersMicroservice.Core.RepositoryInterfaces;
+
+namespace ProjectTirUsersMicroservice.Database.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public User AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
